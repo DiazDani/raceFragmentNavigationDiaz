daed10cd2acd8739b0ec25e87b18162e1e57d0f1
dani.diaz <daniel.diaz@institutvidreres.cat>
2023-11-23 09:33:40 +0100

    Initial commit

---
+++ app/src/main/java/com/sopa/racefragmentnavigation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sopa.racefragmentnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
